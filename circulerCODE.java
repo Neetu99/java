478a11d0f85625fc275551f7a8de0027e18f15e3
neetu <aastha99931@gmail.com>
2022-12-12 15:10:06 +0530

    hi

---
+++ circulerCODE.java
@@ -0,0 +1,16 @@
+public class circulerCODE {
+    public static void main(String[] args) {
+       /*  static void circularRotate(Deque<Integer> deq, int d, int n)
+    {
+        // Push first d elements 
+        // from last to the beginning 
+        for (int i = 0; i < d%n; i++) { 
+            int val = deq.peekLast(); 
+            deq.removeLast(); 
+            deq.addFirst(val); 
+        } 
+    }*/
+//Circular Rotation of Array Using Deque
+    
+}
+}
